3cd7925278e340d2e99427b0d3cd0ef3c3f31ce2
Sarah Spikes <sarah@effectivealtruism.org>
2021-06-16 18:39:13 -0700

    log bid values, update logic

--- texasfortytwo.game.php
+++ texasfortytwo.game.php
@@ -312,7 +312,11 @@ class TexasFortyTwo extends Table {
     $player_id = self::getActivePlayerId();
     // only allow bids higher than current bid if it exists
     $current_bid_value = self::getGameStateValue('bidValue') ;
-    if ((is_null($bid_value) && $bid_value > 30) || $bid_value > $current_bid_value) {
+    self::debug("got bid value: ");
+    self::debug($bid_value);
+    self::debug("current bid value: ");
+    self::debug($current_bid_value);
+    if ((is_null($current_bid_value) && $bid_value >= 30) || $bid_value > $current_bid_value) {
       self::setGameStateValue('bidValue', $bid_value);
       self::setGameStateValue('highestBidder', $player_id);
 
